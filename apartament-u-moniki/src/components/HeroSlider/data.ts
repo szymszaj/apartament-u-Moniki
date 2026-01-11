77fd83065bc962b6c6f84dd78634dc53db5aa105
Szymon Zych <szymonzych868@gmail.com>
2026-01-11 16:45:57 +0100

    new img

--- apartament-u-moniki/src/components/HeroSlider/data.ts
+++ apartament-u-moniki/src/components/HeroSlider/data.ts
@@ -2,5 +2,6 @@ export const heroImages = [
   "https://cf.bstatic.com/xdata/images/hotel/max1024x768/628159546.jpg?k=c98b642e5eea6d09282a179664f7bffc61e821346f4f154352344727ef56eca6&o=&hp=1",
   "/img/kuchnia.jpg",
   "/img/jacuzzi3.jpg",
+  "/img/jacuzzi4.jpg",
   "/img/salon2.jpg",
 ];
